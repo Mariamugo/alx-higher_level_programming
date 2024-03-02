65cca97bf3b9f281ddbba9bd4039b8a20944da53
Mariamugo <shee.mw70@gmail.com>
2024-03-02 09:05:57 +0000

    combination digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for i in range(10):
     for j in range(i+1, 10):
         print('{}'.format(i), end='')
-        print('{},'.format(j), end=' ')
         if i == 8:
-            print(end='\n')
+            print('{}'.format(j), end='\n')
+        else:
+            print('{},'.format(j), end=' ')
