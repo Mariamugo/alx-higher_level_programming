24958b5b90f70aed264ee4b383bb5635a20263d7
Mariamugo <shee.mw70@gmail.com>
2024-04-12 11:49:29 +0000

    print reverse alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,8 @@
-#!/usr/bin/python 3
+#!/usr/bin/python3
+for i in range(ord('z'), ord('A') - 1, -1):
+    if i % 2 == 0:
+        i = chr(i)
+    else:
+        i = chr(i - 32)
+    print('{}'.format(i), end='') 
 
