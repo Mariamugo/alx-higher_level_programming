03d7d9a271871c4e59e70951ef447ab2522a0846
Mariamugo <shee.mw70@gmail.com>
2024-04-19 01:05:04 +0000

    import module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    form calculator_1 import add, mul, sub, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
