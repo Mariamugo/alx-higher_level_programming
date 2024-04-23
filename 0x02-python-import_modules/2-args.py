8c7247cc130705e220d813d47e6e878fddae23d8
Mariamugo <shee.mw70@gmail.com>
2024-04-23 03:42:21 +0000

    print sys.arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    def num_arguments():
+        result = len(sys.argv) - 1
+        if result == 1:
+            print("{} argument:".format(result))
+        elif result == 0:
+            print("{} argument.".format(result))
+        else:
+            print("{} arguments:".format(result))
+        for i in range(1, result):
+            print("{}: {}".format(i, result(i)))
