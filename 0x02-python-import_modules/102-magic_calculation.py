c65aa8880b3142b57ca47b5c056eeb4050bb72b0
Mariamugo <shee.mw70@gmail.com>
2024-05-16 14:10:59 +0000

    magic_calculation bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
+if __name__ == "__main__":
 
-        for i in range(4, 7):
-            c = add(c, i)
-    else:
-        c = sub(a, b)
+    def magic_calculation(a, b):
+        from magic_calculation_102 import add, sub
+        if a < b:
+            c = add(a, b)
 
-    return c 
+            for i in range(4, 7):
+                c = add(c, i)
+        else:
+            c = sub(a, b)
+
+        return c 
